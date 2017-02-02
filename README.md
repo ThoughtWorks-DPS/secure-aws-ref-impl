e31ae2fd3196009ce5bac41fc03c1bf260866c7e
Max Anderson <manderso@thoughtworks.com>
2017-02-01 16:50:18 -0800

    made a dope shell script to automate the Auth0 portion of setting Auth0 up to federate identity

--- README.md
+++ README.md
@@ -11,4 +11,7 @@ by Moritz. Will add more to the README later.
 1. Create admin user through AWS console.
 2. Delete root access keys
 3. Put admin user access keys in ~/.aws/credentials under role 'secure-aws-admin'
-
+4. Get the JSON payload for OpenID Connect auth negotiation from Auth0. If you're signed in, you can
+go to _APIs_ on the menu, then click the _Auth0 Management API_ link, then go to the _Test_ tab.
+The first curl request shown includes a `--data` parameter, the content of which is the necessary payload.
+Save this as auth0payload.json in the root of this directory and you're good to go.
---
+++ auth0idp.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+AUTH0_TENANT=manderso
+test -e auth0payload.json || { echo "Please follow the instructions in the readme for how to get the OpenId Connect payload to authenticate with Auth0."; exit 1; }
+hash brew 2>/dev/null || { echo "INSTALL BREW YOU MONSTER"; exit 1; }
+hash jq 2>/dev/null || brew install jq
+ACCESS_TOKEN=$(curl -H "content-type: application/json" -X POST https://$AUTH0_TENANT.auth0.com/oauth/token -d '@auth0payload.json' | jq -r '.access_token')
+
+APP_ID=$(curl -H "content-type: application/json" -H "authorization: Bearer $ACCESS_TOKEN" -X POST https://manderso.auth0.com/api/v2/clients -d "{\"name\": \"AWS Federated ID\"}" | jq -r '.client_id')
+
+curl -H "content-type: application/json" -H "authorization: Bearer $ACCESS_TOKEN" -X PATCH https://$AUTH0_TENANT.auth0.com/api/v2/clients/$APP_ID -d "{\"callbacks\": [\"https://signin.aws.amazon.com/saml\"],\"addons\": {\"samlp\": $(cat diff.json)}}"
+
+curl -H "content-type: application/json" -H "authorization: Bearer $ACCESS_TOKEN" https://$AUTH0_TENANT.auth0.com/samlp/metadata/$APP_ID > metadata.xml
